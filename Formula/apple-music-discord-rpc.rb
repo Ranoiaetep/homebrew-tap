b84175900b2cc00977f693431fc2b1cad018370c
Ranoiaetep <55516229+Ranoiaetep@users.noreply.github.com>
2023-03-16 05:39:08 -0400

    Update apple-music-discord-rpc.rb

--- Formula/apple-music-discord-rpc.rb
+++ Formula/apple-music-discord-rpc.rb
@@ -1,8 +1,8 @@
 class AppleMusicDiscordRpc < Formula
   desc "Discord Rich Presence client for the macOS Apple Music app"
   homepage "https://github.com/Ranoiaetep/apple-music-discord-rpc"
-  url "https://github.com/Ranoiaetep/apple-music-discord-rpc/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "2823de08fa8521d6c902064160cb3a2d8a7d9b6d0b98eba00916aef63771722f"
+  url "https://github.com/Ranoiaetep/apple-music-discord-rpc/archive/refs/tags/v0.0.1.zip"
+#   sha256 "2823de08fa8521d6c902064160cb3a2d8a7d9b6d0b98eba00916aef63771722f"
   license "MIT"
   head "https://github.com/Ranoiaetep/apple-music-discord-rpc.git", branch: "main"
 
