848ba330c8f4a90f32d95bbfe463e0015b43624c
Ranoiaetep <ranoiaetep@me.com>
2023-03-27 14:24:47 -0400

    Update formulas according to style and audit result

--- Formula/apple-music-discord-rpc.rb
+++ Formula/apple-music-discord-rpc.rb
@@ -1,7 +1,7 @@
 class AppleMusicDiscordRpc < Formula
   desc "Discord Rich Presence client for the macOS Apple Music app"
   homepage "https://github.com/Ranoiaetep/apple-music-discord-rpc"
-  url "https://github.com/Ranoiaetep/apple-music-discord-rpc/archive/refs/tags/v0.0.1.zip"
+  url "https://github.com/Ranoiaetep/apple-music-discord-rpc/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "e409d304cb0f32606da5f5c7392ff7ed9869f6d3402ad3882a7b263153f90144"
   license "MIT"
   head "https://github.com/Ranoiaetep/apple-music-discord-rpc.git", branch: "main"
--- Formula/yt-music.rb
+++ Formula/yt-music.rb
@@ -2,7 +2,6 @@ class YtMusic < Formula
   desc "YouTube Music Downloader"
   homepage "https://github.com/Ranoiaetep/YT-Music"
   url "https://github.com/Ranoiaetep/YT-Music/archive/refs/tags/0.0.1.tar.gz"
-  version "0.0.1"
   sha256 "eec65d5f81fdb233263f69a653450a844b867d1f3e03ea72d7f54f607a4ed7d7"
   license ""
   head "https://github.com/Ranoiaetep/YT-Music.git", branch: "main"
@@ -13,5 +12,4 @@ class YtMusic < Formula
     system "swift", "build", "-c", "release", "--disable-sandbox"
     bin.install ".build/release/yt-music"
   end
-
 end
