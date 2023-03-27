d9039fb6c0ee73f7bbf6902ce1d02dce9fb4dced
Ranoiaetep <ranoiaetep@me.com>
2023-03-27 13:10:29 -0400

    Initial commit

---
+++ Formula/apple-music-discord-rpc.rb
@@ -0,0 +1,22 @@
+class AppleMusicDiscordRpc < Formula
+  desc "Discord Rich Presence client for the macOS Apple Music app"
+  homepage "https://github.com/Ranoiaetep/apple-music-discord-rpc"
+  url "https://github.com/Ranoiaetep/apple-music-discord-rpc/archive/refs/tags/v0.0.1.zip"
+  sha256 "e409d304cb0f32606da5f5c7392ff7ed9869f6d3402ad3882a7b263153f90144"
+  license "MIT"
+  head "https://github.com/Ranoiaetep/apple-music-discord-rpc.git", branch: "main"
+
+  depends_on "deno"
+
+  def install
+    bin.install "music-rpc.ts"
+  end
+
+  service do
+    run bin/"music-rpc.ts"
+    keep_alive true
+    environment_variables PATH: std_service_path_env
+    working_dir var/"apple-music-discord-rpc"
+    error_log_path "music-rpc.log"
+  end
+end
